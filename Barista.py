40b2cc4fc2ba02184e1d793228855aca20d6a11e
DR-Blogs <96393186+DR-Blogs@users.noreply.github.com>
2021-12-19 15:51:48 -0500

    Create Barista.py

---
+++ Barista.py
@@ -0,0 +1,15 @@
+print("Hello, welcome to my coffee shop!!!!")
+
+name = input("What is your name?\n")
+
+print("Hello " + name + ", Thank you so much for coming in today!!\n\n")
+
+menu = "     Black Coffee,\n      Espresso,\n      Latte,\n      Cappucino"
+
+print(name + ", What would you like from our menu today?  Here is what we are serving\n" + menu +"\n")
+
+price = 8
+
+order = input()
+
+quantity = input("How many coffees would you like?\n")
