26a6fef74cc84c320db5ecc06dc83e4e7aec5015
DR-Blogs <96393186+DR-Blogs@users.noreply.github.com>
2021-12-19 15:50:24 -0500

    Create Math.py

---
+++ Math.py
@@ -0,0 +1,18 @@
+name = "Dylan"
+
+age = 31
+actual_age = 31.96
+math = 5 ** 7 + 6 / 9 * 6 -4
+results = age + actual_age + math
+
+print(name)
+print(age)
+
+print(type(name))
+print(type(age))
+print(type(actual_age))
+
+print(5 + 7)
+print(5 ** 7 + 6 / 9 * 6 -4)
+print(math)
+print(results)
